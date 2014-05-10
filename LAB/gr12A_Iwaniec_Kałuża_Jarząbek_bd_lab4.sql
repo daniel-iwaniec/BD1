26a14ebb66e5aec93d3388ef1ee50fbcbc0588c3
Daniel Iwaniec <Ghutix@users.noreply.github.com>
2014-05-10 19:00:59 +0200

    laborki

---
+++ LAB/gr12A_Iwaniec_Kałuża_Jarząbek_bd_lab4.sql
@@ -0,0 +1,26 @@
+SELECT imie, nazwisko, wiek FROM OSOBY;
+
+SELECT nazwisko, telefon FROM OSOBY ORDER BY nazwisko ASC;
+
+SELECT imie, wiek FROM OSOBY ORDER BY wiek DESC;
+
+SELECT ulica FROM adresy WHERE miasto = 'Kielce';
+
+SELECT nazwisko, imie, telefon FROM OSOBY WHERE telefon IS NULL ORDER BY nazwisko ASC;
+
+SELECT nazwisko, imie, telefon FROM OSOBY WHERE telefon IS NOT NULL ORDER BY imie DESC;
+
+SELECT imie, nazwisko, wiek FROM OSOBY WHERE wiek > 30;
+
+SELECT imie, nazwisko, wiek FROM OSOBY WHERE wiek < 40;
+
+SELECT imie, nazwisko, wiek FROM OSOBY WHERE wiek >= 25 AND wiek <= 35;
+
+SELECT nazwisko, imie FROM OSOBY WHERE nazwisko LIKE 'P%' OR nazwisko LIKE 'S%';
+
+SELECT nazwisko, imie FROM OSOBY WHERE regexp_like(nazwisko, '^[A-K]{1}');
+
+SELECT nazwisko, imie FROM OSOBY WHERE NOT regexp_like(nazwisko, '^[A-K]{1}');
+
+SELECT imie, imie, stan_cywilny FROM OSOBY WHERE stan_cywilny != 'wolny' AND stan_cywilny != 'wolna';
+
---
+++ LAB/gr12A_Iwaniec_Kałuża_Jarząbek_bd_lab5.sql
@@ -0,0 +1,5 @@
+SELECT imie, nazwisko, wiek FROM OSOBY;
+
+SELECT nazwisko, telefon FROM OSOBY ORDER BY nazwisko ASC;
+
+SELECT imie, wiek FROM OSOBY ORDER BY nazwisko ASC;
